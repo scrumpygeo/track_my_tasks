f9404c554fd71cd1c8d6847a0088dedd08cb01b8
scrumpygeo <scrumpygeo@yahoo.ca>
2021-02-13 19:29:58 +0000

    add Task component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { useState } from 'react'
 import Header from '../src/components/Header'
-function App() {
+import Tasks from '../src/components/Tasks'
+
+const App = () => {
+  // create state and use dummy data as default
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Vet Appointment',
+      day: 'Feb 16th at 10:15am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Gardener Appointment',
+      day: 'Feb 17th at 09:15am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Car Service',
+      day: 'Feb 18th at 12:15am',
+      reminder: false,
+    },
+  ])
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   )
 }
